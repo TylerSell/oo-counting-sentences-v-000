48b494d8504ebf2427912cd4c6e1db75d0717d85
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-03 19:24:55 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,18 +5,24 @@ class String
   def sentence?
     if self.end_with?(".")
       true 
+    else 
+      false 
     end
   end
 
   def question?
-    if self.end_with("?")
+    if self.end_with?("?")
       true 
+    else 
+      false 
     end
   end
 
   def exclamation?
     if self.end_with?("!")
       true 
+    else 
+      false 
     end
   end
 
